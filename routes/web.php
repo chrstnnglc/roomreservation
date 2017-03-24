1c31adff360cf270648cc33cbe7eaf30b13458cc
avatarchristine <catfelizardo@gmail.com>
2017-03-24 18:07:24 +0800

    Rerouted in line with new controllers and views

--- routes/web.php
+++ routes/web.php
@@ -13,95 +13,72 @@
 // Get routes for PagesController
 Auth::routes();
 
-Route::get('/', 'PagesController@login'); // dapat index pero sige ito muna
-Route::get('login', 'PagesController@login');
-Route::get('success', 'PagesController@success');
-Route::get('register', 'PagesController@register');
+Route::get('/', 'PagesController@index'); // dapat index pero sige ito muna
+// Route::get('login', 'PagesController@login');
+// Route::get('success', 'PagesController@success');
+// Route::get('register', 'PagesController@register');
 // Route::get('home', 'PagesController@yes');
+
 Route::get('reserve_form', 'PagesController@reserve_form');
 Route::get('reserve', 'PagesController@reserve');
 Route::get('user', 'PagesController@user');
 Route::get('edit_user', 'PagesController@edit_user');
 
 // Get routes for AdminController
-Route::group(['middleware' => 'admin'], function () {
-	Route::get('admin/rooms', 'AdminController@rooms');
-	// Route::get('admin/addroom', 'AdminController@addform');
-	Route::get('admin/rooms/{room}', 'AdminController@showroom');
-	Route::post('admin/rooms', 'AdminController@addroom');
-	Route::get('admin/rooms/{room}/editroom', 'AdminController@editroom');
-	Route::post('admin/rooms/{room}','AdminController@deleteroom');
-	Route::put('admin/rooms/{room}','AdminController@updateroom');
 
-	Route::get('admin/reservations', 'AdminController@reservations');
-	Route::post('admin/reservations', 'AdminController@addreservation');
-	Route::get('admin/reserve_form', 'AdminController@reserve_form');
+Route::get('rooms', 'RoomsController@index');
+Route::get('rooms/{room}', 'RoomsController@showroom');
+Route::post('rooms', 'RoomsController@addroom');
+Route::get('rooms/{room}/editroom', 'RoomsController@editroom');
+Route::post('rooms/{room}','RoomsController@deleteroom');
+Route::put('rooms/{room}','RoomsController@updateroom');
 
-	Route::get('admin/equipment', 'AdminController@equipment');
-	Route::post('admin/equipment', 'AdminController@addequipment');
-	Route::get('admin/equipments/{equipment}', 'AdminController@showequipment');
-	Route::get('admin/equipments/{equipment}/editequipment', 'AdminController@editequipment');
-	Route::put('admin/equipments/{equipment}','AdminController@updateequipment');
-	Route::post('admin/equipments/{equipment}','AdminController@deleteequipment');
+Route::get('equipment', 'EquipmentsController@index');
+Route::post('equipment', 'EquipmentsController@addequipment');
+Route::get('equipments/{equipment}', 'EquipmentsController@showequipment');
+Route::get('equipments/{equipment}/editequipment', 'EquipmentsController@editequipment');
+Route::put('equipments/{equipment}','EquipmentsController@updateequipment');
+Route::post('equipments/{equipment}','EquipmentsController@deleteequipment');
 
-	Route::get('admin/user', 'AdminController@user');
-	Route::post('admin/user', 'AdminController@adduser');
-	Route::get('admin/user/{user}', 'AdminController@showuser');
-	Route::get('admin/user/{user}/edituser', 'AdminController@edituser');
-	Route::put('admin/user/{user}','AdminController@updateuser');
-	Route::post('admin/user/{user}','AdminController@deleteuser');
-	
-	Route::get('admin/logs', 'AdminController@logs');
-	Route::get('admin/payments', 'AdminController@payment');
-});
+Route::get('reservations', 'ReservationsController@index');
+Route::post('reservations', 'ReservationsController@addreservation');
+Route::get('reserve_form', 'ReservationsController@reserve_form');
 
-#Route::group(['middleware' => 'media'], function () {
-Route::get('media/rooms', 'MediaController@rooms');
-Route::get('media/rooms/{room}', 'MediaController@showroom');
-Route::post('media/rooms', 'MediaController@addroom');
-Route::get('media/rooms/{room}/editroom', 'MediaController@editroom');
-Route::post('media/rooms/{room}','MediaController@deleteroom');
-Route::put('media/rooms/{room}','MediaController@updateroom');
+Route::get('user', 'UserController@userslist');
+Route::post('user', 'UserController@adduser');
+Route::get('user/{user}', 'UserController@showuser');
+Route::get('user/{user}/edituser', 'UserController@edituser');
+Route::put('user/{user}','UserController@updateuser');
+Route::post('user/{user}','UserController@deleteuser');
 
-Route::get('media/reservations', 'MediaController@reservations');
-Route::get('media/reserve_form', 'MediaController@reserve_form');
+// #Route::group(['middleware' => 'media'], function () {
+// Route::get('media/rooms', 'MediaController@rooms');
+// Route::get('media/rooms/{room}', 'MediaController@showroom');
+// Route::post('media/rooms', 'MediaController@addroom');
+// Route::get('media/rooms/{room}/editroom', 'MediaController@editroom');
+// Route::post('media/rooms/{room}','MediaController@deleteroom');
+// Route::put('media/rooms/{room}','MediaController@updateroom');
 
-Route::get('media/equipments', 'MediaController@equipment');
-Route::post('media/equipments', 'MediaController@addequipment');
-Route::get('media/equipments/{equipment}', 'MediaController@showequipment');
-Route::get('media/equipments/{equipment}/editequipment', 'MediaController@editequipment');
-Route::put('media/equipments/{equipment}','MediaController@updateequipment');
-Route::post('media/equipments/{equipment}','MediaController@deleteequipment');
+// Route::get('media/reservations', 'MediaController@reservations');
+// Route::get('media/reserve_form', 'MediaController@reserve_form');
 
-Route::get('media/user', 'MediaController@user');
-Route::post('media/user', 'MediaController@adduser');
-Route::get('media/user/{user}', 'MediaController@showuser');
-Route::get('media/user/{user}/edituser', 'MediaController@edituser');
-Route::put('media/user/{user}','MediaController@updateuser');
-Route::post('media/user/{user}','MediaController@deleteuser');
+// Route::get('media/equipments', 'MediaController@equipment');
+// Route::post('media/equipments', 'MediaController@addequipment');
+// Route::get('media/equipments/{equipment}', 'MediaController@showequipment');
+// Route::get('media/equipments/{equipment}/editequipment', 'MediaController@editequipment');
+// Route::put('media/equipments/{equipment}','MediaController@updateequipment');
+// Route::post('media/equipments/{equipment}','MediaController@deleteequipment');
 
-Route::get('media/logs', 'MediaController@logs');
-#});
+// Route::get('media/user', 'MediaController@user');
+// Route::post('media/user', 'MediaController@adduser');
+// Route::get('media/user/{user}', 'MediaController@showuser');
+// Route::get('media/user/{user}/edituser', 'MediaController@edituser');
+// Route::put('media/user/{user}','MediaController@updateuser');
+// Route::post('media/user/{user}','MediaController@deleteuser');
+
+// Route::get('media/logs', 'MediaController@logs');
+// #});
 
 
 Route::get('treasury/reservations', 'TreasuryController@reservations');
 Route::patch('treasury/reservation/{reserve}', 'TreasuryController@updatereservation');
-
-/*
-
-Route::get('store', 'PokemonsController@index');
-Route::get('store/{pokemon}', 'PokemonsController@show');
-Route::get('store/order', 'PokemonsController@order');
-Route::get('add', 'PokemonsController@addform');
-Route::get('store/{pokemon}/edit', 'PokemonsController@editform');
-Route::post('store', 'PokemonsController@addpokemon');
-Route::put('store/{pokemon}','PokemonsController@editpokemon');
-
-*/
-
-// Route::get('child', function(){
-//     return view('child');
-// });
-// Route::get('test', function(){
-//     return view('test');
-// });
