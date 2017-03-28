2f740cf2dabb43a7e0b1b0bfb8e81870d7de619a
Tammy Mapua <Tammy Mapua>
2017-03-29 06:36:27 +0800

    Edited home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,13 @@
 <title>Diocese of Cubao Reservation System</title>
 @stop
 @section('items')
-@if (Auth::user() !== NULL)
+@if (Auth::user() != NULL)
 <a class="item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
 <a class="item" style="font-size: 110%" href = "{{url('/user')}}">Users</a>
 <a class="item" style="font-size: 110%" href = "{{url('/equipment')}}">Equipment</a>
 <a class="item" style="font-size: 110%" href = "{{url('/rooms')}}">Rooms</a>
 <a class="item" style="font-size: 110%" href = "{{url('/logs')}}">Logs</a>
+<a class="item" style="font-size: 110%" href = "{{url('profile')}}">{{Auth::user()->username}}</a>
 @endif
 @stop
 
