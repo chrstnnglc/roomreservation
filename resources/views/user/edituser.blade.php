8da6926fddc7f6092d8bdfd233aab68d535a14c5
rddelosreyes <delosreyes.robend@gmail.com>
2017-04-12 11:35:19 +0800

    Fixed user

--- resources/views/user/edituser.blade.php
+++ resources/views/user/edituser.blade.php
@@ -1,20 +1,31 @@
-@extends('admin.master')
+@extends('layouts.master')
 @section('name')
 <title>Equipment - Diocese of Cubao Reservation System</title>
 @stop
 @section('items')
-<a class="item" style="font-size: 110%" href = "{{url('/admin/reservations')}}">Reservations</a>
-<a class="item" style="font-size: 110%" href = "{{url('/admin/user')}}">Users</a>
-<a class="active item" style="font-size: 110%" href = "{{url('/admin/equipment')}}">Equipment</a>
-<a class="item" style="font-size: 110%" href = "{{url('/admin/rooms')}}">Rooms</a>
-<a class="item" style="font-size: 110%" href = "{{url('/admin/logs')}}">Logs</a>
+<a class="item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
+<a class="item" style="font-size: 110%" href = "{{url('/user')}}">Users</a>
+<a class="active item" style="font-size: 110%" href = "{{url('/equipment')}}">Equipment</a>
+<a class="item" style="font-size: 110%" href = "{{url('/rooms')}}">Rooms</a>
+<a class="item" style="font-size: 110%" href = "{{url('/logs')}}">Logs</a>
 @stop
 @section('content')
 <div class="ui middle aligned center aligned grid">
   <div class="column">
-	<h1>User</h1>				
-
-	    <form class="ui large form" method="POST" action="/admin/user/{{ $user->id }}">
+	<h1>Edit User</h1>				
+		
+		@if (count($errors) > 0)
+		<div class = "ui left aligned inverted red stacked segment">
+			<i class="warning icon"></i>
+			Can't update!
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li class = "">{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+		@endif
+	    <form class="ui large form" method="POST" action="/user/{{ $user->id }}">
 	        {{ method_field('PUT') }}
 	        {{ csrf_field() }}
 	        <div class="ui yellow stacked segment">
@@ -40,10 +51,16 @@
 			  	<input type="text" name="affiliation" value="{{ $user->affiliation }}">
 			  </div>
 			  <div class="field">
-			  	<input type="text" name="users_role" value="{{ $user->users_role }}">
-			  </div>
+		      <select name="users_role" class="ui dropdown" id="select">
+		        <option value=""></option>
+		        <option value="admin">Admin</option>
+		        <option value="media">Media</option>
+		        <option value="treasury">Treasury</option>
+		        <option value="user">User</option>
+		      </select>
+		    </div>
 	        </div>
-	        <a href="{{url('/admin/user')}}">
+	        <a href="{{url('/user')}}">
 	        	<button class="ui yellow fluid button" type="submit">Update</button>
 	    	</a>
 	    </form>
