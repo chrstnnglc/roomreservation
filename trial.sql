545c204311d7901f2719f1ebbabac79394eef8fc
Christine Felizardo <chrstnnglc@users.noreply.github.com>
2017-04-26 20:31:20 +0800

    Delete trial.sql

--- trial.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Rooms (name, capacity, rate) VALUES ('Room1', 50, 5000);
-INSERT INTO Rooms (name, capacity, rate) VALUES ('Room2', 20, 1000);
-
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment1', 'Brand1', 'Model1', 1000, 'working', 1);
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment2', 'Brand2', 'Model2', 2000, 'working', 1);
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment3', 'Brand3', 'Model3', 3000, 'working', 1);
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment1', 'Brand1', 'Model1', 1000, 'working', 2);
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment2', 'Brand2', 'Model2', 2000, 'working', 2);
-INSERT INTO Equipment (name, brand, model, price, condition, room_id) VALUES ('Equipment3', 'Brand3', 'Model3', 3000, 'working', 2);
