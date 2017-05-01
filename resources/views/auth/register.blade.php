75d9ce85b88004e639ccdacd64b5ffefed88109c
Tammy Mapua <Tammy Mapua>
2017-05-01 20:15:29 +0800

    Used master.blade.php for its layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,57 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <!-- Standard Meta -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
-  <!-- Site Properties -->
+@extends('layouts.master')
+  @section('name')
   <title>Register - Diocese of Cubao Reservation System</title>
-
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/semantic.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/calendar.min.css') }}">
-
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/divider.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/segment.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/form.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/input.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/button.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/list.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/message.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/icon.css') }}">
-
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-  <script src="{{asset('/js/semantic.min.js')}}"></script>
-
-  <style type="text/css">
-    body {
-      background-color: #DADADA;
-      overflow: auto;
-    }
-    body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
-    }
-    .column {
+  @stop
+  @section('width')
       max-width: 450px;
-    }
-  </style>
-</head>
-<body>
-  <div class="ui secondary top fixed inverted blue borderless menu">
-    <div class="container" style="padding: 10px 0 10px 10px;">
-      <img src="12logo.png">
-    </div>
-    <div class="container" style="padding: 10px 0 10px 10px;">
-    <a href="{{url('/')}}"><h3 class="header item">Room Reservation System</h3></a>
-    </div>
-  </div>
-
-<div class="ui middle aligned center aligned grid">
-  <div class="column">
+  @stop
+@section('content')
     <h2>
       <div class="content">
         Sign up for your account!
@@ -149,5 +103,4 @@
         </div>
       </div>
   </form>
-  </body>
-  </html>
+@stop
