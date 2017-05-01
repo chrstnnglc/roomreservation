848d576c19cb8c7c73ecda8e5beba600dba8f4bd
chrstnnglc <catfelizardo@gmail.com>
2017-05-01 18:30:16 +0800

    Rerouted login to auth/login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <!-- Standard Meta -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+@extends('layouts.master')
+@section('name')
+<title>Login - Diocese of Cubao Reservation System</title>
+@stop
+@section('width')
+max-width: 50%;
+@stop
 
-  <!-- Site Properties -->
-  <title>Login - Diocese of Cubao Reservation System</title>
-
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/semantic.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/calendar.min.css') }}">
-
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/divider.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/segment.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/form.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/input.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/button.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/list.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/message.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('/css/icon.css') }}">
-
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-  <script src="{{asset('/js/semantic.min.js')}}"></script>
-
-  <style type="text/css">
-    body {
-      background-color: #DADADA;
-      overflow: auto;
-    }
-    body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
-    }
-    .column {
-      max-width: 25%;
-    }
-  </style>
-</head>
-<body>
-  <div class="ui secondary top fixed inverted blue borderless menu">
-    <div class="container" style="padding: 10px 0 10px 10px;">
-      <img src="12logo.png">
-    </div>
-    <div class="container" style="padding: 10px 0 10px 10px;">
-    <a href="{{url('/')}}"><h3 class="header item">Room Reservation System</h3></a>
-    </div>
-  </div>
-
+@section('content')
   <div class="ui middle aligned center aligned grid">
     <div class="column">
       <h2>
@@ -59,6 +16,19 @@
       </h2>
       <form class="ui large form" method = "POST" action = "{{url('/login')}}" >
       {{ csrf_field() }}
+          
+          @if (count($errors) > 0)
+          <div class = "ui left aligned inverted red stacked segment">
+              <!--<i class="warning icon"></i>
+              Can't add!-->
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li class = "">{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+          @endif
+        
         <div class="ui yellow stacked segment">
           <div class="field">
             <div class="ui left icon input">
@@ -84,7 +54,4 @@
       </div>
     </div>
   </div>
-
-</body>
-
-</html>
+@stop
