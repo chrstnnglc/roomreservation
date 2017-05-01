196586b1708ac110179557a7db02e1b827631c66
Neal Bryan Velasco <bonchyvelasco@outlook.com>
2017-05-01 17:51:14 +0800

    Fixed error.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,6 +25,11 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
     /**
      * Where to redirect users after login.
      *
