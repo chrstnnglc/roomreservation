0db33413b061adb5c78d71f6bc1c91f1b1582992
chrstnnglc <catfelizardo@gmail.com>
2017-05-01 18:47:49 +0800

    Added terms and conditions text box

--- resources/views/reservations/form.blade.php
+++ resources/views/reservations/form.blade.php
@@ -79,12 +79,16 @@ max-width: 50%;
 	      </div>
 	    </div>
 
-	    <!-- <div class="field">
+	    <textarea>
+	    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium nunc eu libero fermentum, vulputate vehicula urna vulputate. Cras dui turpis, aliquam vitae sodales vel, imperdiet eget ligula. Nunc in cursus sem, vitae suscipit quam. Curabitur auctor nibh ac condimentum lobortis. Duis eget arcu eu quam posuere volutpat. Sed non ultrices tellus. Proin auctor nulla feugiat, gravida elit nec, commodo dolor. Aliquam in tincidunt lacus. Phasellus fringilla porttitor lorem et finibus. Vivamus non mauris sit amet elit vulputate porta. Integer id bibendum lorem. Duis eget nulla sed orci blandit sodales ac nec erat. Ut fermentum tortor turpis, id congue mi volutpat a. Aenean sed vestibulum eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+	    </textarea>
+
+	    <div class="field">
 	    	<div class="ui slider checkbox">
 			  <input type="checkbox" name="terms">
 			  <label>I accept the terms and conditions.</label>
 			</div>
-		</div> -->
+		</div>
 
 		<div class="container" align="center">
 	    <div class="container" style="width: 50%;">
