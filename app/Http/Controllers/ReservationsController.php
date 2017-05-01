c59c86c655e32e0b9028887a13c4c76df7f77665
rddelosreyes <delosreyes.robend@gmail.com>
2017-05-01 20:15:36 +0800

    Inluded ingress and egress time in hours count

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -59,11 +59,11 @@ class ReservationsController extends Controller
         $reserve->date_reserved = $request->date;
         $reserve->start_of_reserved = date("H:i:s", strtotime($request->starttime) - 3600);
         $reserve->end_of_reserved = date("H:i:s", strtotime($request->endtime) + 1800);
-        if ((strtotime($request->endtime) - strtotime($request->starttime))/3600 < 0){
-            $reserve->hours = (strtotime($request->endtime)+(24*60*60) - strtotime($request->starttime))/3600;
+        if ((strtotime($reserve->end_of_reserved) - strtotime($reserve->start_of_reserved))/3600 < 0){
+            $reserve->hours = (strtotime($reserve->end_of_reserved)+(24*60*60) - strtotime($reserve->start_of_reserved))/3600;
         }
         else{
-            $reserve->hours = (strtotime($request->endtime) - strtotime($request->starttime))/3600;
+            $reserve->hours = (strtotime($reserve->end_of_reserved) - strtotime($reserve->start_of_reserved))/3600;
         }
         $reserve->price = ($reserve->hours * $room->rate > 0)?$reserve->hours * $room->rate:$room->rate;
         $equipment_reserved = Equipment::where('room_id', $room->id);
