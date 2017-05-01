a2293fa203f1e1c9d2ccfd93028915961e8fe55e
Neal Bryan Velasco <bonchyvelasco@outlook.com>
2017-05-01 20:45:43 +0800

    Added validation rules to ReservationsController

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -54,7 +54,7 @@ class ReservationsController extends Controller
             'roomname' => "required|exists:rooms,name",
             'date' => "required|date|after_or_equal:" . date("Y-m-d"),
             'starttime' => "required",
-            'endtime' => "required"
+            'endtime' => "required|greater_than_field:starttime"
 
             ]);
 
@@ -69,7 +69,7 @@ class ReservationsController extends Controller
             'roomname' => "required|exists:rooms,name",
             'date' => "required|date|after_or_equal:" . date("Y-m-d"),
             'starttime' => "required",
-            'endtime' => "required|greater_than_field"
+            'endtime' => "required|greater_than_field:starttime"
 
             ]);
 
