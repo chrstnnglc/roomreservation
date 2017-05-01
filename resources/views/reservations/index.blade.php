594a036028fb350356d390077a48bec7553b8bac
Tammy Mapua <Tammy Mapua>
2017-05-01 14:19:50 +0800

    Fixed doubling of table

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -108,27 +108,3 @@ max-width: 60%;
 @stop
 
 <!-- User, Room, date of res, date reserved, starttime, endtime, hours, price, date_paid-->
-
-@extends('layouts.master')
-@section('name')
-<title>Reservations - Diocese of Cubao Reservation System</title>
-@stop
-@section('width')
-max-width: 60%;
-@stop
-@section('items')
-@if (Auth::user()->users_role == 'admin' or Auth::user()->users_role == 'media')
-<a class="active item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
-<a class="item" style="font-size: 110%" href = "{{url('/user')}}">Users</a>
-<a class="item" style="font-size: 110%" href = "{{url('/equipment')}}">Equipment</a>
-<a class="item" style="font-size: 110%" href = "{{url('/rooms')}}">Rooms</a>
-<a class="item" style="font-size: 110%" href = "{{url('/logs')}}">Logs</a>
-<a class="item" style="font-size: 110%" href = "{{url('/user/profile')}}">{{Auth::user()->username}}</a>
-@elseif (Auth::user()->users_role == 'treasury')
-<a class="active item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
-<a class="item" style="font-size: 110%" href = "{{url('/user/profile')}}">{{Auth::user()->username}}</a>
-@elseif (Auth::user()->users_role == 'user')
-<a class="active item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
-<a class="item" style="font-size: 110%" href = "{{url('/user/profile')}}">{{Auth::user()->username}}</a>
-@endif
-@stop
