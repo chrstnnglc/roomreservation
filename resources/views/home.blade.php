770ed2d4b4865fe0261351b984f5b9196338b804
chrstnnglc <catfelizardo@gmail.com>
2017-05-02 01:26:19 +0800

    Allowed user to view room and equipment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,8 @@ max-width: 90%;
 <a class="item" style="font-size: 110%" href = "{{url('/user/profile')}}">{{Auth::user()->username}}</a>
 @elseif (Auth::user()->users_role == 'user')
 <a class="item" style="font-size: 110%" href = "{{url('/reservations')}}">Reservations</a>
+<a class="item" style="font-size: 110%" href = "{{url('/equipment')}}">Equipment</a>
+<a class="item" style="font-size: 110%" href = "{{url('/rooms')}}">Rooms</a>
 <a class="item" style="font-size: 110%" href = "{{url('/user/profile')}}">{{Auth::user()->username}}</a>
 @endif
 @endif
